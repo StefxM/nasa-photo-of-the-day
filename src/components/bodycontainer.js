416bba84ac0eedcf5853c470c2eba3a83fac841b
stephanie mairena <stefyem7711@gmail.com>
2020-03-03 21:06:21 -0500

    filesetup

---
+++ src/components/bodycontainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./components.css";
+
+//container for pics, try to display three at a time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*REMEMBER TO LINK BACK TO APP.JS
---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./components.css";
+
+//time in space pics were taken, and whats is in pictures i want three pics, but if not two so need two columns of info underneath
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*REMEMBER TO LINK BACK TO APP.JS
